c2cdb4085f3e02e1481c77dedbdb4cd11916f021
tateyama-hiroaki <user@USERnoMacBook-Air.local>
2022-05-13 02:38:58 +0900

    close #20 update: Updated article controller test

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  # http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
   
   def index
     @articles = Article.all
@@ -48,6 +48,6 @@ class ArticlesController < ApplicationController
   private
 
     def article_params
-      params.require(:article).premit(:title, :body, :status)
+      params.require(:article).permit(:title, :body, :status)
     end
 end
--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,51 @@
 require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @article = articles(:first_article)
+  end
+
+  teardown do
+    Rails.cache.clear
+  end
+
+  test "should get index" do
+    get articles_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get article_url(@article)
+    assert_response :success  
+  end
+
+  test "should get new" do
+    get new_article_url
+    assert_response :success    
+  end
+
+  test "should create article" do
+    assert_difference("Article.count") do
+      post articles_url, params: { article: { title: "hello", body: "Hello, world.", status: "public" } }  
+    end    
+  end
+
+  test "should get edit" do
+    get edit_article_url(@article)
+    assert_response :success
+  end
+
+  test "should update article" do
+    patch article_url(@article), params: { article: { title: "hello" } }
+    assert_redirected_to article_url(@article)
+    @article.reload
+    assert_equal "hello", @article.title
+  end
+
+  test "should destroy article" do
+    assert_difference("Article.count", -1) do
+      delete article_url(@article)
+    end
+    assert_redirected_to root_url
+  end
 end
