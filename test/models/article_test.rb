b8b868b389199e8eab31325931585fb6cb55faed
tateyama-hiroaki <user@USERnoMacBook-Air.local>
2022-05-14 20:01:27 +0900

    add: Add article model test

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @article = articles(:first_article)
+  end
+
+  teardown do
+    Rails.cache.clear
+  end
+
+  test "should be valid" do
+    assert @article.valid?
+  end
+
+  test "title should be present" do
+    @article.title = ""
+    assert_not @article.valid?
+  end
+
+  test "body should be present" do
+    @article.body = ""
+    assert_not @article.valid?
+  end
+
+  test "body should not be too short" do
+    @article.body = "Hey!"
+    assert_not @article.valid?
+  end
 end
