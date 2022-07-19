3d9bef9bef579609aba4ba702fe9e9cfff25c9c7
tateyama-hiroaki <user@USERnoMacBook-Air.local>
2022-07-20 03:05:30 +0900

    Created comments controller test

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @article = articles(:first)
+    @comment = @article.comments.create(commenter: "Bob", body: "Hello, Ruby on Rails.", status: "public")
+  end
+
+  def teardown
+    Rails.cache.clear
+  end
+
+  test "Did you succeed in creating a comment?" do
+    assert_difference("@article.comments.count") do
+      post article_comments_url(@article), params: { comment: { commenter: "Alice", body: "Hello, Ruby.", status: "public" } }
+    end 
+
+    assert_redirected_to @article
+  end
+
+  test "Did you succeed in destroy a comment?" do
+    assert_difference("@article.comments.count", -1) do
+      delete article_comment_url(@article, @comment)
+    end
+
+    assert_redirected_to @article
+  end
 end
